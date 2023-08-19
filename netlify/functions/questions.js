5cd8b68769b44327878610299923ca1d1a54ad50
Naman Yadav <raymannky2001@gmail.com>
2023-08-19 16:58:17 +0530

    setuping the server for fake api

--- netlify/functions/questions.js
+++ netlify/functions/questions.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const server = express();
+const path = require("path"); // Import the path module
 
 server.get("/questions", (req, res) => {
-  const questions = require("../data/questions.json");
+  const questions = require(path.join(__dirname, "../../questions.json")); // Adjust the path
   res.json(questions);
 });
 
